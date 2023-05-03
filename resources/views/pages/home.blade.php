c03d5619b7ec0439125d1404c30362dac50397a6
Alvin Otuya <alvin@hisa.co>
2023-05-03 16:47:01 +0300

    home page clean up

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -168,122 +168,6 @@
 
     </section>
 
-
-
-    {{-- <section class="pb-5 in-avo-1">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card border-0">
-                        <div class="card-body d-flex flex-column flex-md-row justify-content-between align-items-center">
-                            <div class="col-md-3 col-lg-auto border-end-lg">
-                                <div class="pe-lg-3">
-                                    <h6 class="mb-md-0 mb-lg-0">Various trusted media</h6>
-                                    <p class="text-muted mb-0 d-none d-lg-block">have covered us</p>
-                                </div>
-                            </div>
-                            <div class="col-md-9 col-lg-10">
-                                <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 d-flex align-items-center gx-0">
-                                    <div class="col text-center">
-                                        <img class="opacity-75 my-2 my-md-1 my-lg-1" src="{{asset('avo-assets/img/in-avo-press-1.svg')}}" alt="client-logo" width="127" height="19">
-                                    </div>
-                                    <div class="col text-center">
-                                        <img class="opacity-75 my-2 my-md-1 my-lg-1" src="{{asset('avo-assets/img/in-avo-press-2.svg')}}" alt="client-logo" width="126" height="23">
-                                    </div>
-                                    <div class="col text-center d-none d-lg-block">
-                                        <img class="opacity-75 my-2 my-md-1 my-lg-1" src="{{asset('avo-assets/img/in-avo-press-3.svg')}}" alt="client-logo" width="155" height="23">
-                                    </div>
-                                    <div class="col text-center d-none d-lg-block">
-                                        <img class="opacity-75 my-2 my-md-1 my-lg-1" src="{{asset('avo-assets/img/in-avo-press-4.svg')}}" alt="client-logo" width="102" height="29">
-                                    </div>
-                                    <div class="col text-center">
-                                        <img class="opacity-75 my-2 my-md-1 my-lg-1" src="{{asset('avo-assets/img/in-avo-press-5.svg')}}" alt="client-logo" width="124" height="29">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> --}}
-    <!-- section content end -->
-    <!-- section content begin -->
-    <section class="py-5 in-avo-2">
-        <div class="container">
-            <div class="row row-cols-md-3 row-cols-lg-4 text-center mb-5">
-                <div class="col">
-                    <h1 class="mb-0">
-                        <span class="count text-black" data-counter-end="183092">183092</span><span class="text-primary">+</span>
-                    </h1>
-                    <p class="text-muted">Daily trades</p>
-                </div>
-                <div class="col">
-                    <h1 class="mb-0">
-                        <span class="count text-black" data-counter-end="364887">364887</span><span class="text-primary">+</span>
-                    </h1>
-                    <p class="text-muted">Clients</p>
-                </div>
-                <div class="col d-md-none d-lg-block">
-                    <h1 class="mb-0">
-                        <span class="count text-black" data-counter-end="96">96</span> billion<span class="text-primary">+</span>
-                    </h1>
-                    <p class="text-muted">Asset management</p>
-                </div>
-                <div class="col">
-                    <h1 class="mb-0">
-                        <span class="count text-black" data-counter-end="23">23</span> billion<span class="text-primary">+</span>
-                    </h1>
-                    <p class="text-muted">Daily trade volume</p>
-                </div>
-            </div>
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 d-flex align-items-center text-center mt-md-4 gy-3 gy-md-0">
-                <div class="col">
-                    <div class="icon-wrap bg-primary rounded-circle flex-shrink-0 mb-2">
-                        <img src="{{asset('avo-assets/img/in-avo-2-icon-1.png')}}" alt="icon" width="38" height="38">
-                    </div>
-                    <h4 class="text-black">Various assets/</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elited do eiusmod tempo officios incididunt.</p>
-                </div>
-                <div class="col active-col">
-                    <div class="icon-wrap bg-primary rounded-circle flex-shrink-0 mb-2">
-                        <img src="{{asset('avo-assets/img/in-avo-2-icon-2.png')}}" alt="icon" width="38" height="38">
-                    </div>
-                    <h4 class="text-white">Market analysis</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elited do eiusmod tempo officios incididunt.</p>
-                </div>
-                <div class="col d-md-none d-lg-block">
-                    <div class="icon-wrap bg-primary rounded-circle flex-shrink-0 mb-2">
-                        <img src="{{asset('avo-assets/img/in-avo-2-icon-3.png')}}" alt="icon" width="38" height="38">
-                    </div>
-                    <h4 class="text-black">Enhanced tools</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elited do eiusmod tempo officios incididunt.</p>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- section content end -->
-    <!-- section content begin -->
-    <section class="pb-5 in-avo-3">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="col-md-12 col-lg-7">
-                        <h2 class="text-black">Getting started with shares in Kenya on the <span class="text-highlight text-black">Hisa App</span>  </h2>
-
-                    </div>
-                    <div class="card border-0 bg-hisa">
-
-                        <div class="card-body d-flex flex-column flex-md-row justify-content-between align-items-center">
-
-                            <div class="ratio ratio-16x9">
-                                <iframe width="738" height="415" src="https://www.youtube.com/embed/ax3TYhupQMM" title="Investing 101: Getting started with shares in Kenya on the Hisa App" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>                              </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>                    
-    </section>   
     <section class="pt-1 pb-5 pt-md-2 pb-md-5 py-lg-5 in-avo-3">
 
         <div class="col-md-12 d-flex flex-column align-items-center">
@@ -561,6 +445,122 @@
             
         </div>
     </section>
+
+    {{-- <section class="pb-5 in-avo-1">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card border-0">
+                        <div class="card-body d-flex flex-column flex-md-row justify-content-between align-items-center">
+                            <div class="col-md-3 col-lg-auto border-end-lg">
+                                <div class="pe-lg-3">
+                                    <h6 class="mb-md-0 mb-lg-0">Various trusted media</h6>
+                                    <p class="text-muted mb-0 d-none d-lg-block">have covered us</p>
+                                </div>
+                            </div>
+                            <div class="col-md-9 col-lg-10">
+                                <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 d-flex align-items-center gx-0">
+                                    <div class="col text-center">
+                                        <img class="opacity-75 my-2 my-md-1 my-lg-1" src="{{asset('avo-assets/img/in-avo-press-1.svg')}}" alt="client-logo" width="127" height="19">
+                                    </div>
+                                    <div class="col text-center">
+                                        <img class="opacity-75 my-2 my-md-1 my-lg-1" src="{{asset('avo-assets/img/in-avo-press-2.svg')}}" alt="client-logo" width="126" height="23">
+                                    </div>
+                                    <div class="col text-center d-none d-lg-block">
+                                        <img class="opacity-75 my-2 my-md-1 my-lg-1" src="{{asset('avo-assets/img/in-avo-press-3.svg')}}" alt="client-logo" width="155" height="23">
+                                    </div>
+                                    <div class="col text-center d-none d-lg-block">
+                                        <img class="opacity-75 my-2 my-md-1 my-lg-1" src="{{asset('avo-assets/img/in-avo-press-4.svg')}}" alt="client-logo" width="102" height="29">
+                                    </div>
+                                    <div class="col text-center">
+                                        <img class="opacity-75 my-2 my-md-1 my-lg-1" src="{{asset('avo-assets/img/in-avo-press-5.svg')}}" alt="client-logo" width="124" height="29">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> --}}
+    <!-- section content end -->
+    <!-- section content begin -->
+    <section class="py-5 in-avo-2">
+        <div class="container">
+            <div class="row row-cols-md-3 row-cols-lg-4 text-center mb-5">
+                <div class="col">
+                    <h1 class="mb-0">
+                        <span class="count text-black" data-counter-end="183092">183092</span><span class="text-primary">+</span>
+                    </h1>
+                    <p class="text-muted">Daily trades</p>
+                </div>
+                <div class="col">
+                    <h1 class="mb-0">
+                        <span class="count text-black" data-counter-end="364887">364887</span><span class="text-primary">+</span>
+                    </h1>
+                    <p class="text-muted">Clients</p>
+                </div>
+                <div class="col d-md-none d-lg-block">
+                    <h1 class="mb-0">
+                        <span class="count text-black" data-counter-end="96">96</span> billion<span class="text-primary">+</span>
+                    </h1>
+                    <p class="text-muted">Asset management</p>
+                </div>
+                <div class="col">
+                    <h1 class="mb-0">
+                        <span class="count text-black" data-counter-end="23">23</span> billion<span class="text-primary">+</span>
+                    </h1>
+                    <p class="text-muted">Daily trade volume</p>
+                </div>
+            </div>
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 d-flex align-items-center text-center mt-md-4 gy-3 gy-md-0">
+                <div class="col">
+                    <div class="icon-wrap bg-primary rounded-circle flex-shrink-0 mb-2">
+                        <img src="{{asset('avo-assets/img/in-avo-2-icon-1.png')}}" alt="icon" width="38" height="38">
+                    </div>
+                    <h4 class="text-black">Various assets/</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elited do eiusmod tempo officios incididunt.</p>
+                </div>
+                <div class="col active-col">
+                    <div class="icon-wrap bg-primary rounded-circle flex-shrink-0 mb-2">
+                        <img src="{{asset('avo-assets/img/in-avo-2-icon-2.png')}}" alt="icon" width="38" height="38">
+                    </div>
+                    <h4 class="text-white">Market analysis</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elited do eiusmod tempo officios incididunt.</p>
+                </div>
+                <div class="col d-md-none d-lg-block">
+                    <div class="icon-wrap bg-primary rounded-circle flex-shrink-0 mb-2">
+                        <img src="{{asset('avo-assets/img/in-avo-2-icon-3.png')}}" alt="icon" width="38" height="38">
+                    </div>
+                    <h4 class="text-black">Enhanced tools</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elited do eiusmod tempo officios incididunt.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- section content end -->
+    <!-- section content begin -->
+    <section class="pb-5 in-avo-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="col-md-12 col-lg-7">
+                        <h2 class="text-black">Getting started with shares in Kenya on the <span class="text-highlight text-black">Hisa App</span>  </h2>
+
+                    </div>
+                    <div class="card border-0 bg-hisa">
+
+                        <div class="card-body d-flex flex-column flex-md-row justify-content-between align-items-center">
+
+                            <div class="ratio ratio-16x9">
+                                <iframe width="738" height="415" src="https://www.youtube.com/embed/ax3TYhupQMM" title="Investing 101: Getting started with shares in Kenya on the Hisa App" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>                              </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>                    
+    </section>   
+
 
 
     <!-- second embed section -->
